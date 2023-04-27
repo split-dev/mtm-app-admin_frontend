d76781ecad9d7c86dcb49bd657133d921c9622d5
Анастасия Найденко <anastasianajdenko@MBP-Anastasia.N4>
2023-04-27 21:12:05 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mtm-app-admin-core-ui';
+}
