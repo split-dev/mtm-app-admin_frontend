f47d8d40b93ee23b9044477f243cf14637de1ee9
IVaN-KaLNYTSKYI <68811887+IVaN-KaLNYTSKYI@users.noreply.github.com>
2023-12-08 13:27:46 +0200

    layout-abjustment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -8,3 +8,4 @@ export const environment = {
 };
 //https://mtm-app-api.netlify.app/.netlify/functions/api/v1
 //http://localhost:3000/api/v1
+//https://nn18txbxlk.execute-api.eu-north-1.amazonaws.com/dev/api/v1
